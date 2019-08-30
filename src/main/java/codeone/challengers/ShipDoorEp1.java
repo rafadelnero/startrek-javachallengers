17b631bd47c035b9582735ed2d2eb5863bd0e347
Rinaldo Pitzer Júnior <16694899+rinaldodev@users.noreply.github.com>
2019-08-30 10:23:49 -0300

    Create ShipDoorEp1.java

---
+++ src/main/java/codeone/challengers/ShipDoorEp1.java
@@ -0,0 +1,22 @@
+package desafios.autocloseable;
+
+public class ShipDoorEp1 implements AutoCloseable {
+  public void open() {
+    throw new RuntimeException("Fail to open door.");
+  }
+  
+  @Override
+  public void close() throws Exception {
+    System.out.println("Ship door is closed.");
+  }
+  
+  public static void main(String... doorMalfunction) {
+    try (ShipDoorEp1 shipDoor = new ShipDoorEp1()) {
+      shipDoor.open();
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+    } finally {
+      System.out.println("The end.");
+    }
+  }
+}
