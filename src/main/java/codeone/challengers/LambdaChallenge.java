12268cba3e400eff018596e0f9aa5e3a8901abf0
Rinaldo Pitzer Júnior <16694899+rinaldodev@users.noreply.github.com>
2019-08-30 10:25:19 -0300

    Create LambdaChallenge.java

---
+++ src/main/java/codeone/challengers/LambdaChallenge.java
@@ -0,0 +1,16 @@
+package codeone.challengers;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class LambdaChallenge {
+
+  public static void main(String... theDoors) {
+    int doorNumber = 0;
+    List<String> doors = Arrays.asList("A", "B", "C");
+    doors.forEach(e -> {
+      System.out.println(e + doorNumber++);
+    });
+  }
+
+}
