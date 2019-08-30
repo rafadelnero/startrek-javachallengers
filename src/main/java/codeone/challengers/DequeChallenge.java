20d646ae17578bbde50364b08da8e4850d020a10
Rinaldo Pitzer Júnior <16694899+rinaldodev@users.noreply.github.com>
2019-08-30 10:25:37 -0300

    Create DequeChallenge.java

---
+++ src/main/java/codeone/challengers/DequeChallenge.java
@@ -0,0 +1,22 @@
+package codeone.challengers;
+
+import java.util.Collections;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class DequeChallenge {
+
+  public static void main(String... confusedCrew) {
+    Deque<String> crew = new LinkedList<String>();
+    crew.addFirst("Worf");
+    crew.addFirst("Odo");
+    crew.addLast("Scott");
+    
+    Queue<String> crewQueue = Collections.asLifoQueue(crew);
+    System.out.println(crewQueue.poll());
+    System.out.println(crewQueue.poll());
+    System.out.println(crewQueue.poll());
+  }
+  
+}
