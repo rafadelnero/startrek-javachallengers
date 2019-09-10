67631483d568402524f72b2870c5f5f34df7c219
Paul <sdjugpres@yahoo.com>
2019-09-09 18:30:58 -0700

    Cleaning up and find the IntelliJ is wrong!

--- src/main/java/codeone/challengers/WrathOfLambdaVar.java
+++ src/main/java/codeone/challengers/WrathOfLambdaVar.java
@@ -1,32 +1,18 @@
 package codeone.challengers;
 //JDK 11 Challenge (still drafting)
 public class WrathOfLambdaVar {
-    public static void main(String... varGroup){
-        System.out.println(new WrathOfLambdaVar().localVars());
+    public static void main(String... cetiAlphaSystem){
+        System.out.println(new WrathOfLambdaVar().localPlanets());
     }
 
-    private int localVars(){
-        ITest test = (String s1, String s2) -> s1 + s2;
-        ITest test1 = (s1, final s2) -> s1 + s2;
-        ITest test2 = (var s1, final var s2) -> s1 + s2;
-        // look at class since var is var and the interface has a String type
-        //reflect on the type
-        test2.test("x",null);
-//        ITest test3 = (var s1, @Nullable var s2) -> s1 + s2;
-//        test3.test("x",null);
-        // not good
-        //ITest test4 = (var s1, s2) -> s1 + s2;
-        //ITest test5 = (var s1, String s2) -> s1 + s2;
-        ITest2 test6 = s1 -> s1.toUpperCase(); // this one is okay
-        //ITest2 test7 = var s1 -> s1.toUpperCase();
-        return 0;
+    private String localPlanets(){
+        ISpaceSeed whereIsKhan = (String cetiAlphaV, String cetiAlphaVI) -> cetiAlphaV + cetiAlphaVI;
+        ISpaceSeed whenWorldsCollide  = (s1, final s2) -> s1 + s2;
+        ISpaceSeed reliantsMistake = (var s1, final var s2) -> s1 + s2;
+        return reliantsMistake.collide("Ceti Alpha 6",null);
     }
     @FunctionalInterface
-    interface ITest{
-        String test(String s1, String s2);
-    }
-    @FunctionalInterface
-    interface ITest2{
-        String test2(String s1);
+    interface ISpaceSeed {
+        String collide(String s1, String s2);
     }
 }
