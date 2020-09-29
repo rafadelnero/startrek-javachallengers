39c4cd41b83348f3f7912c0f961e1943b8bd3563
rafadelnero <rafacdelnero@gmail.com>
2020-09-29 09:42:02 +0100

    - Added Text block challenge

---
+++ src/main/java/codeone/challengers/TextBlockChallenge.java
@@ -0,0 +1,21 @@
+package codeone.challengers;
+
+public class TextBlockChallenge {
+
+  public static void main(String... doYourBest) {
+    String jediWithoutTextBlock = ""
+        + "<html>\n"
+        + "    <head>Master Yoda</head> %s\n"
+        + "</html>"
+        .strip().formatted("Luke");
+
+    String jediWithTextBlock = """
+        <html>
+            <head>Master Yoda</head> %s
+        </html>
+        """.strip().formatted("Luke");
+
+    System.out.println(jediWithoutTextBlock.equals(jediWithTextBlock));
+    System.out.println(jediWithoutTextBlock.formatted("Luke").equals(jediWithTextBlock));
+  }
+}
