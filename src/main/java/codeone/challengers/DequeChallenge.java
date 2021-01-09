0445286dfb9e8291b3984357ca2e5d50307a6780
rafaeldelnero <rafael.delnero@fabfitfun.com>
2021-01-09 17:05:54 -0300

    - Changed to Java 15. - Added extra logic into the TurnaboutIntruder challenge.

--- src/main/java/codeone/challengers/DequeChallenge.java
+++ src/main/java/codeone/challengers/DequeChallenge.java
@@ -1,22 +1,21 @@
 package codeone.challengers;
 //Planet JDK 6 - The Confused Crew
-import java.util.Collections;
+
 import java.util.Deque;
 import java.util.LinkedList;
-import java.util.Queue;
 
 public class DequeChallenge {
 
   public static void main(String... confusedCrew) {
     Deque<String> crew = new LinkedList<String>();
     crew.addFirst("Worf");
-    crew.addFirst("Odo");
+    crew.push("Odo");
     crew.addLast("Scott");
-    
-    Queue<String> crewQueue = Collections.asLifoQueue(crew);
-    System.out.println(crewQueue.poll());
-    System.out.println(crewQueue.poll());
-    System.out.println(crewQueue.poll());
+
+    System.out.println(crew.remove());
+    System.out.println(crew.peek());
+    System.out.println(crew.pop());
+    System.out.println(crew.poll());
   }
   
 }
--- src/main/java/codeone/challengers/TurnaboutIntruder.java
+++ src/main/java/codeone/challengers/TurnaboutIntruder.java
@@ -1,4 +1,8 @@
 package codeone.challengers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 //Planet JDK 10 – Var Turnabout intruder
 public class TurnaboutIntruder {
     public static void main(String... awayTeam){
@@ -6,11 +10,16 @@ public class TurnaboutIntruder {
     }
     private boolean whoIsKirk() {
         Var var = new Var();
-        var cmdSpock = new Var(){};
-        var cptKirk = new Var(){boolean isVar(){return true;}};
+        var cptKirk = new Var(){ boolean isVar(){ return true; } };
         var drLester = (Var) null;
         drLester = cptKirk;
 
+        var numbers = new ArrayList<Integer>(Arrays.asList(1,2,3));
+
+        System.out.println(numbers);
+
+        numbers.remove(1);
+        System.out.println(numbers);
         return cptKirk.isVar();
     }
     private static class Var {}
