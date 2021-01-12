6c6ef3d5e21e7f22291a913104fd2fd75ad6116d
rafaeldelnero <rafael.delnero@fabfitfun.com>
2021-01-12 17:27:59 -0300

    - Added sealed class challenge

--- src/main/java/codeone/challengers/SealedStarTrekChallenge.java
+++ src/main/java/codeone/challengers/SealedStarTrekChallenge.java
@@ -5,21 +5,22 @@ package codeone.challengers;
  * Permitted subclass must explicitly extend the sealed class.
  * Permitted subclass must define a modifier: sealed, non-sealed or final.
  * Permitted subclass must belong to the same module to the sealed class.
+ * Can't create anonymous inner class from a sealed interface or class.
  */
 public class SealedStarTrekChallenge {
   sealed interface Captain permits Spock, Saru {
     default void shoot() { System.out.println("A captain will shoot!"); }
   }
-  static non-sealed class Saru implements Captain {}
+  static non-sealed class Saru implements Captain {} // #A
 
-  record Spock() implements Captain {
-    public void shoot() { System.out.println("Spock shoots!"); }
+  record Spock(String gun) implements Captain { // #B
+    public void shoot() { System.out.println("Spock shoots with:" + gun); }
   }
 
   public static void main(String... doYourBest) {
-    shoot(new Spock());
+    shoot(new Spock("Type-1 Phaser"));
     shoot(new Saru());
-    shoot("Dooh!");
+    shoot((Spock) new Object());
   }
 
   static void shoot(Object captain) {
@@ -32,3 +33,17 @@ public class SealedStarTrekChallenge {
     }
   }
 }
+
+/*
+    A - Line #A won't compile
+
+    B - Spock shoots with:Type-1 Phaser
+        A captain will shoot!
+        java.lang.ClassCastException will be thrown
+
+    C - Line #B won't compile
+
+    D - A captain will shoot!
+        A captain will shoot!
+        java.lang.RuntimeException will be thrown
+ */
