0ee04b062a9d242f03749bbe64fca34657063821
rafaeldelnero <rafael.delnero@fabfitfun.com>
2021-01-11 22:22:34 -0300

    - Changed var challenge

--- src/main/java/codeone/challengers/TurnaboutIntruder.java
+++ src/main/java/codeone/challengers/TurnaboutIntruder.java
@@ -4,23 +4,50 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 //Planet JDK 10 – Var Turnabout intruder
+/*
+ Rules of var
+  - No instance variables
+  - No method parameters
+  - No lambdas assigning
+  - Only local variables
+  - No null assigning
+ */
 public class TurnaboutIntruder {
     public static void main(String... awayTeam){
-        System.out.println(new TurnaboutIntruder().whoIsKirk());
-    }
-    private boolean whoIsKirk() {
-        Var var = new Var();
-        var cptKirk = new Var(){ boolean isVar(){ return true; } };
-        var drLester = (Var) null;
-        drLester = cptKirk;
+        var var = new Var() { public String collide(String c1, String c2) { return  c1 + c2; }};
+        var drLester = (Var) null; // Line 4
+        drLester = var;
 
-        var numbers = new ArrayList<Integer>(Arrays.asList(1,2,3));
+        Var reliantsMistake = (var s1, final var s2) -> String.join(";", s1, s2, "Darth Vader");
 
+        var numbers = new ArrayList<>(Arrays.asList(3, 2, 1));
+        numbers.remove(Integer.valueOf(1));
+
+        System.out.println(var.collide("Kirk", "Spock"));
+        System.out.println(reliantsMistake.collide("Khan", "Borg"));
+        System.out.println(drLester.getClass().getName());
         System.out.println(numbers);
-
-        numbers.remove(1);
-        System.out.println(numbers);
-        return cptKirk.isVar();
     }
-    private static class Var {}
+
+    private interface Var { String collide(String c1, String c2); }
 }
+
+/*
+  Alternatives:
+  a - Compilation error at line 4
+
+  b - KirkSpock
+      Khan;Borg;Darth Vader
+      codeone.challengers.TurnaboutIntruder$1
+      [3, 2]
+
+  c - KirkSpock
+      ;KhanBorgDarth Vader
+      codeone.challengers.Var
+      [2, 1]
+
+   d - SpockKirk
+       Khan;Borg;Darth Vader
+       codeone.challengers.Var
+       [3, 1]
+ */
