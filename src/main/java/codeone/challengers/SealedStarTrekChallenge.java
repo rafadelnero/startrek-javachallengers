c7cf93be0cbfc7696cb4397a68dde8f7f10e14bf
rafaeldelnero <rafael.delnero@fabfitfun.com>
2021-01-16 09:53:51 -0300

    - Finished this Java Challenge.

--- src/main/java/codeone/challengers/SealedStarTrekChallenge.java
+++ src/main/java/codeone/challengers/SealedStarTrekChallenge.java
@@ -6,16 +6,18 @@ package codeone.challengers;
  * Permitted subclass must define a modifier: sealed, non-sealed or final.
  * Permitted subclass must belong to the same module to the sealed class.
  * Can't create anonymous inner class from a sealed interface or class.
+ *
+ * This a compilation error challenge, that's why parts of the code is commented.
  */
 public class SealedStarTrekChallenge {
 
-  sealed interface Captain permits Spock, Saru {
+   sealed interface Captain permits Spock, Saru/*, Janeway*/ { // #A
     default void giveOrder() {
-      System.out.println("Retrieve!");
+      System.out.println("Attack!");
     }
   }
-
-  static non-sealed class Saru implements Captain {} // #A
+  static non-sealed class Saru implements Captain {}
+  static class Janeway {}
 
   record Spock(String planetName) implements Captain { // #B
     public void giveOrder() { System.out.println("Let's go to:" + planetName); }
@@ -24,21 +26,21 @@ public class SealedStarTrekChallenge {
   public static void main(String... doYourBest) {
     Captain captain = new Spock("Vulcan");
     captain.giveOrder();
-    Saru saru = new Saru();
-    saru.giveOrder();
+    System.out.println(captain.getClass().getSuperclass().isSealed());
+    new Saru().giveOrder();
+//    new Captain() { }.giveOrder(); // #C
   }
 }
 
 /*
-    A - Line #A won't compile
+    A - Line #A and #B  won't compile
 
-    B - Spock shoots with:Type-1 Phaser
-        A captain will shoot!
-        java.lang.ClassCastException will be thrown
+    B - Line #A and #C won't compile
 
-    C - Line #B won't compile
+    C - Let's go to:Vulcan
+        true
+        Attack!
+        Attack!
 
-    D - A captain will shoot!
-        A captain will shoot!
-        java.lang.RuntimeException will be thrown
+    D - Line #A, #B and #C won't compile
  */
