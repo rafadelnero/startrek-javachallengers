76732ff7ce7ae42b2d9efdc7ed0b2075e7b302fb
rafadelnero <rafacdelnero@gmail.com>
2022-09-21 21:45:18 +0100

    - Added Sealed challenge

--- src/main/java/devoxx/challenges/StructureConcurrency.java
+++ src/main/java/devoxx/challenges/StructureConcurrency.java
@@ -1,42 +1,44 @@
-import jdk.incubator.concurrent.StructuredTaskScope;
-import java.util.Random;
-import java.util.concurrent.Future;
+package devoxx.challenges;
+
 import java.util.concurrent.ExecutionException;
-public class Main {
+import java.util.concurrent.Future;
+import jdk.incubator.concurrent.StructuredTaskScope;
 
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
-        int result = test1() + test2();
-        System.out.println("Result = " + result);
-    }
+public class StructureConcurrency {
 
-    private static int test1() throws ExecutionException, InterruptedException  {
-        try (var scope = new StructuredTaskScope.ShutdownOnSuccess<Integer>()) {
-            Future<Integer> f1 = scope.fork(Main::get1);
-            Future<Integer> f2 = scope.fork(Main::get2);
-            scope.join();
-            return scope.result();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-    }
+  public static void main(String[] args) throws ExecutionException, InterruptedException {
+    int result = test1() + test2();
+    System.out.println("Result = " + result);
+  }
 
-    private static int test2() throws ExecutionException, InterruptedException  {
-        try (var scope = new StructuredTaskScope.ShutdownOnFailure()) {
-            Future<Integer> f1 = scope.fork(Main::get1);
-            Future<Integer> f2 = scope.fork(Main::get2);
-            scope.join();
-            return f1.resultNow() + f2.resultNow();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+  private static int test1() throws ExecutionException {
+    try (var scope = new StructuredTaskScope.ShutdownOnSuccess<Integer>()) {
+      Future<Integer> f1 = scope.fork(StructureConcurrency::get1);
+      Future<Integer> f2 = scope.fork(StructureConcurrency::get2);
+      scope.join();
+      return scope.result();
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
     }
+  }
 
-    private static int get1() throws InterruptedException {
-	Thread.sleep(5000);
-        return 1;
+  private static int test2() {
+    try (var scope = new StructuredTaskScope.ShutdownOnFailure()) {
+      Future<Integer> f1 = scope.fork(StructureConcurrency::get1);
+      Future<Integer> f2 = scope.fork(StructureConcurrency::get2);
+      scope.join();
+      return f1.resultNow() + f2.resultNow();
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
     }
+  }
 
-    private static int get2() {
-        return 2;
-    }
+  private static int get1() throws InterruptedException {
+    Thread.sleep(5000);
+    return 1;
+  }
+
+  private static int get2() {
+    return 2;
+  }
 }
--- src/main/java/devoxx/challenges/VirtualThreads.java
+++ src/main/java/devoxx/challenges/VirtualThreads.java
@@ -1,9 +1,13 @@
+package devoxx.challenges;
+
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+
 public class VirtualThreads {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("---------");
         try (ExecutorService e = Executors.newVirtualThreadPerTaskExecutor()) {
+            Thread.sleep(2000);
             e.submit(() -> System.out.println("1"));
             e.submit(() -> System.out.println("2"));
         }
