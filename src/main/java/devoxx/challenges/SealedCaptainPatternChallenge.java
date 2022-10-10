a21a17b6c57bb5c1bcc825e0cbeed0ba5adcbe4b
rafadelnero <rafacdelnero@gmail.com>
2022-10-10 22:58:55 +0200

    Fixed SealedCaptainPatternChallenge.

--- src/main/java/devoxx/challenges/SealedCaptainPatternChallenge.java
+++ src/main/java/devoxx/challenges/SealedCaptainPatternChallenge.java
@@ -1,24 +1,30 @@
 package devoxx.challenges;
 
 public class SealedCaptainPatternChallenge {
-  sealed interface Captain permits Saru, Spock/*, Janeway*/ {}
+  sealed interface Captain permits Saru, Spock/*, Janeway */ {} // #A
   static non-sealed class Saru implements Captain {}
   record Spock(String planetName) implements Captain {}
-//  static class Janeway implements Captain {}
+  //static class Janeway implements Captain {} // #B
 
   public static void main(String... doYourBest) {
     command(new Saru());
     command(new Spock("Vulcan"));
-//    command(new Janeway());
+    //command(new Captain() {});   // #C
   }
 
   static void command(Captain captain) {
     switch (captain) {
       case Saru saru -> System.out.println(saru.getClass().isSealed());
       case Captain spock -> System.out.println(spock.getClass().isRecord());
-//      case Janeway janeway -> System.out.println(janeway.getClass().getSimpleName());
+//      case Janeway janeway ->   // #D
+//        System.out.println(janeway.getClass().getSimpleName());
     }
   }
 }
+
+// A - #A, #B and #C won't compile
+// B - #A and #B won't compile
+// C - #A, #B, #C and #D won't compile
+// D - #B and #D won't compile
 
 // All sealed class subclasses must either be final, sealed or non-sealed
